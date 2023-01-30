b4dbdb488a1bb7135019b4f3621cee4abeb26da7
Amanda Bienz <bienz2@users.noreply.github.com>
2023-01-30 11:10:47 -0700

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,13 +1,14 @@
 #include "src.hpp"
 
-// Priority Scheduling -- Jobs with highest priority (lowest number) run first
+// Priority Scheduling 
+//   -- Jobs with highest priority (lowest number) run first
 //   -- If multiple jobs with same priority, lowest index runs first
 void priority(int n_jobs, Job* jobs)
 {
 }
 
-// Priority Scheduling with Round Robin -- Jobs with highest priority (lowest number)
-//    run first
+// Priority Scheduling with Round Robin 
+//    -- Jobs with highest priority (lowest number) run first
 //    -- If multiple jobs have same priority, run all in round robin 
 //    -- Time slice for round robin passed as a parameter
 void priority_rr(int n_jobs, Job* jobs, int time_slice)
